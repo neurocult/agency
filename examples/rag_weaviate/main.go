ca4343f5887e63b0e22bab4126e25eeac8403fc1
Emil Valeev <emil.musician@gmail.com>
2023-11-20 22:56:49 +0600

    feat(examples:rag): turn weaviate example into rag example with tts

--- examples/rag_weaviate/main.go
+++ examples/rag_weaviate/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"os"
 
+	"github.com/eqtlab/lib/core"
+	"github.com/eqtlab/lib/openai"
+	"github.com/eqtlab/lib/pipeline"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/weaviate/weaviate-go-client/v4/weaviate"
 	"github.com/weaviate/weaviate-go-client/v4/weaviate/graphql"
 	"github.com/weaviate/weaviate/entities/models"
 )
 
+// natural langauge query -> weaviate RAG -> speech
 func main() {
 	openAPIKey := os.Getenv("OPENAI_API_KEY")
 
@@ -21,32 +25,57 @@ func main() {
 		panic(err)
 	}
 
-	fields := []graphql.Field{{Name: "content"}}
+	factory := openai.New(openAPIKey)
+	rag := RAGPipe(client)
+	stt := factory.TextToSpeech(openai.TextToSpeechParams{
+		Model: "tts-1", ResponseFormat: "mp3", Speed: 1, Voice: "onyx",
+	})
 
-	result, err := client.GraphQL().Get().
-		WithClassName("Records").
-		WithFields(fields...).
-		WithNearText(
-			client.GraphQL().
-				NearTextArgBuilder().
-				WithConcepts([]string{"programming"}),
-		).
-		WithLimit(10).
-		Do(ctx)
+	result, err := pipeline.New(rag, stt).Execute(ctx, core.NewUserMessage("programming"))
 	if err != nil {
 		panic(err)
 	}
 
-	// factory := openai.New(openAPIKey)
-	// pipeline.New(
-	// 	factory.TextToText(openai.TextToTextParams{
-	// 		Model: "gpt3dot5turbo",
-	// 	}).WithOptions(core.WithPrompt("")),
-	// )
+	if err := saveToDisk(result); err != nil {
+		panic(err)
+	}
+}
 
-	// TODO add TTS example with pipes
+// RAGPipe retrieves relevant objects from vector store and builds a text message to pass further to the pipeline
+func RAGPipe(client *weaviate.Client) *core.Pipe {
+	return core.NewPipe(func(ctx context.Context, msg core.Message, po ...core.PipeOption) (core.Message, error) {
+		input := msg.String()
 
-	fmt.Println("query about programming", result)
+		result, err := client.GraphQL().Get().
+			WithClassName("Records").
+			WithFields(graphql.Field{Name: "content"}).
+			WithNearText(
+				client.GraphQL().
+					NearTextArgBuilder().
+					WithConcepts(
+						[]string{input},
+					),
+			).
+			WithLimit(10).
+			Do(ctx)
+		if err != nil {
+			panic(err)
+		}
+
+		var content string
+		for _, obj := range result.Data {
+			bb, err := json.Marshal(&obj)
+			if err != nil {
+				return nil, err
+			}
+			content += string(bb)
+		}
+
+		return core.TextMessage{
+			Role:    core.AssistantRole,
+			Content: content,
+		}, nil
+	})
 }
 
 func prepareDB(openAPIKey string, ctx context.Context) (*weaviate.Client, error) {
@@ -82,4 +111,19 @@ func prepareDB(openAPIKey string, ctx context.Context) (*weaviate.Client, error)
 	}
 
 	return client, nil
+}
+
+func saveToDisk(msg core.Message) error {
+	file, err := os.Create("example.mp3")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(msg.Bytes())
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
